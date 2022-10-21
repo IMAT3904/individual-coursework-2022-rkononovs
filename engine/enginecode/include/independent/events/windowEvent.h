c60d3da2027a172fa15a165597c3d6da28008881
Romans <50585282+rkononovs@users.noreply.github.com>
2022-10-21 22:41:39 +0300

    Added window events

--- engine/enginecode/include/independent/events/windowEvent.h
+++ engine/enginecode/include/independent/events/windowEvent.h
@@ -26,4 +26,35 @@ namespace Engine {
 		inline int getWidth() const { return m_width; }
 		inline int getHeight() const { return m_height; }
 	};
+
+	class WindowFocusEvent : public Event {
+	public:
+		WindowFocusEvent() {}
+		
+		EventType getEventType() const override { return EventType::WindowFocus; } //!< Get the event type
+		int getCategoryFlags() const override { return EventCategoryWindow; } //!< Get the event category flags
+	};
+
+	class WindowLostFocusEvent : public Event {
+	public:
+		WindowLostFocusEvent() {}
+
+		EventType getEventType() const override { return EventType::WindowLostFocus; } //!< Get the event type
+		int getCategoryFlags() const override { return EventCategoryWindow; } //!< Get the event category flags
+	};
+
+	class WindowMovedEvent : public Event {
+	private:
+		int m_xPos;
+		int m_yPos;
+	public:
+		WindowMovedEvent(int x, int y) : m_xPos(x), m_yPos(y) {}
+
+		EventType getEventType() const override { return EventType::WindowMoved; } //!< Get the event type
+		int getCategoryFlags() const override { return EventCategoryWindow; } //!< Get the event category flags
+
+		inline int getXPos() const { return m_xPos; }
+		inline int getYPos() const { return m_yPos; }
+		inline glm::ivec2 getPos() const { return { m_xPos, m_yPos }; }
+	};
 }
