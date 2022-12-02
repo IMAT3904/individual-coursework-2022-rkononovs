eadcbd1e735fde952365561de444382d761445a3
Romans <50585282+rkononovs@users.noreply.github.com>
2022-12-03 01:32:54 +0200

    Started to abstract buffer

---
+++ engine/enginecode/include/independent/rendering/bufferLayout.h
@@ -0,0 +1,63 @@
+/** \file bufferLayout.h */
+#pragma once
+
+#include <vector>
+
+#include "shaderDataType.h"
+
+namespace Engine {
+
+	/**
+	\class BufferElement
+	A class which hold a single element in a buffer layout
+	*/
+	class BufferElement {
+	public:
+		ShaderDataType m_dataType; //!< The type of data
+		int m_size; //!< The size of data
+		int m_offset; //!< The offset of data
+		bool m_normalized; //!< Normalization of data
+
+		BufferElement() {} //!< Default constructor
+		BufferElement(ShaderDataType dataType, bool normalized = false) :
+			m_dataType(dataType),
+			m_size(STD::size(dataType)),
+			m_offset(0),
+			m_normalized(normalized){} //!< Constructor with parameters
+	};
+
+	/** \class BufferLayout
+	* Abstraction of the buffer layout
+	*/
+	class BufferLayout {
+	private:
+		std::vector<BufferElement> m_elements; //!< Buffer elements
+		int m_stride; //!< Width in bytes of the buffer line
+		void calcStrideAndOffset(); //!< Calculate stride and offsets based on elements 
+	public:
+		BufferLayout() {};
+		BufferLayout(const std::initializer_list<BufferElement>& element) : m_elements(element) { calcStrideAndOffset(); }
+		inline int getStride() { return m_stride; }
+		void addElement(BufferElement element);
+		inline std::vector<BufferElement>::iterator begin() { return m_elements.begin(); }
+		inline std::vector<BufferElement>::iterator end() { return m_elements.end(); }
+		inline std::vector<BufferElement>::const_iterator begin() const { return m_elements.begin(); }
+		inline std::vector<BufferElement>::const_iterator end() const { return m_elements.end(); }
+	};
+
+	void BufferLayout::addElement(BufferElement element) {
+		m_elements.push_back(element);
+		calcStrideAndOffset();
+	}
+
+	void BufferLayout::calcStrideAndOffset() {
+		int offset = 0;
+
+		for (auto& element : m_elements) {
+			element.m_offset = offset;
+			offset += element.m_size;
+		}
+
+		m_stride = offset;
+	}
+}
---
+++ engine/enginecode/include/independent/rendering/shaderDataType.h
@@ -0,0 +1,32 @@
+/** \file shaderDataType.h */
+#pragma once
+
+#include <cstdint>
+
+namespace Engine {
+	enum class ShaderDataType {
+		None = 0, Float, Float2, Float3, Float4
+	};
+
+	namespace STD {
+		static int size(ShaderDataType type) {
+			switch (type) {
+			case ShaderDataType::Float: return 4;
+			case ShaderDataType::Float2: return 4 * 2;
+			case ShaderDataType::Float3: return 4 * 3;
+			case ShaderDataType::Float4: return 4 * 4;
+			default: return 0;
+			}
+		}
+
+		static int componentCount(ShaderDataType type) {
+			switch (type) {
+			case ShaderDataType::Float: return 1;
+			case ShaderDataType::Float2: return 2;
+			case ShaderDataType::Float3: return 3;
+			case ShaderDataType::Float4: return 4;
+			default: return 0;
+			}
+		}
+	}
+}
