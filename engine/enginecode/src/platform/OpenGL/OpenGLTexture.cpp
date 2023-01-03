28cea4160e6f72ba9c95836053bec2ed8e532a05
Romans <50585282+rkononovs@users.noreply.github.com>
2023-01-04 00:41:38 +0200

    Added monochrome channel for textures

--- engine/enginecode/src/platform/OpenGL/OpenGLTexture.cpp
+++ engine/enginecode/src/platform/OpenGL/OpenGLTexture.cpp
@@ -32,6 +32,11 @@ namespace Engine {
 		if (data) {
 			if (m_channels == 3) glTextureSubImage2D(GL_TEXTURE_2D, 0, xOffset, yOffset, width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
 			else if (m_channels == 4) glTextureSubImage2D(GL_TEXTURE_2D, 0, xOffset, yOffset, width, height, GL_RGBA, GL_UNSIGNED_BYTE, data);
+			else if (m_channels == 1) {
+				glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+				glTextureSubImage2D(GL_TEXTURE_2D, 0, xOffset, yOffset ,width, height, GL_RED, GL_UNSIGNED_BYTE, data);
+				glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+			}
 		}
 	}
 
@@ -48,6 +53,11 @@ namespace Engine {
 
 		if (channels == 3) glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 		else if (channels == 4) glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		else if (channels == 1) {
+			glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, data);
+			glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+		}
 		else return;
 		glGenerateMipmap(GL_TEXTURE_2D);
 
